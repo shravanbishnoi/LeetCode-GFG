6c4733d7c8dc25d297a0e1abd6708ed019063c58
Shravan Bishnoi <116088301+shravanbishnoi@users.noreply.github.com>
2024-08-31 23:07:23 +0530

    Create IncreasingTripletSubsequence.py

---
+++ IncreasingTripletSubsequence.py
@@ -0,0 +1,33 @@
+"""
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+"""
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        first = second = float('inf')
+        
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        
+        return False
